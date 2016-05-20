e07adc427da148291e5ce50f9ef4c73133a9f1d3
Edgardo Pinto-Escalier Scoffe <ep-marketing-media@live.com>
2016-05-20 21:48:07 +0200

    Added code to display results in the form

--- js/script.js
+++ js/script.js
@@ -27,5 +27,10 @@ function submitAnswers() {
 		}
 	}
 	
+	//Here we display the results
+	var results = document.getElementById('results');
+	results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>'
 	alert('You scored '+score+' out of '+total);
+
+	return false;
 }
